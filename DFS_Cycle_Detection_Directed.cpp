16f505b556263d8657ccbb4551501914126ae108
MatthewBieda <matthew.bieda@gmail.com>
2024-08-29 11:17:17 +0100

    Cleaner version

--- DFS_Cycle_Detection_Directed.cpp
+++ DFS_Cycle_Detection_Directed.cpp
@@ -1,75 +1,70 @@
-// We must keep track of the recursion stack to determine whether the node is revisited in the same DFS path
-// which indicates the presence of a cycle
-
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 class Graph {
-    int V; // Number of vertices
-    vector<vector<int>> adjList; // Adjacency list
+private:
+    std::vector<std::vector<int>> adjList;
+    std::vector<bool> visited;
+    std::vector<bool> inRecStack; // To keep track of nodes in the recursion stack
 
-    bool dfs(int v, vector<bool> &visited, vector<bool> &recStack);
+    bool dfs(int node) {
+        // If the node is currently in the recursion stack, we found a cycle
+        if (inRecStack[node]) {
+            return true;
+        }
+        // If the node is already visited, no need to process it again
+        if (visited[node]) {
+            return false;
+        }
+
+        // Mark the node as visited and add it to the recursion stack
+        visited[node] = true;
+        inRecStack[node] = true;
+
+        // Visit all adjacent nodes
+        for (int neighbor : adjList[node]) {
+            if (dfs(neighbor)) {
+                return true;
+            }
+        }
+
+        // Remove the node from the recursion stack and continue
+        inRecStack[node] = false;
+        return false;
+    }
 
 public:
-    Graph(int V);
-    void addEdge(int v, int w);
-    bool hasCycle();
+    Graph(int vertices) : adjList(vertices), visited(vertices, false), inRecStack(vertices, false) {}
+
+    void addEdge(int u, int v) {
+        adjList[u].push_back(v);
+    }
+
+    bool hasCycle() {
+        for (int i = 0; i < adjList.size(); ++i) {
+            if (!visited[i]) {
+                if (dfs(i)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 };
 
-Graph::Graph(int V) {
-    this->V = V;
-    adjList.resize(V);
-}
-
-void Graph::addEdge(int v, int w) {
-    adjList[v].push_back(w);
-}
-
-bool Graph::dfs(int v, vector<bool> &visited, vector<bool> &recStack) {
-    visited[v] = true;
-    recStack[v] = true;
-
-    for (int u : adjList[v]) {
-        if (!visited[u]) {
-            if (dfs(u, visited, recStack)) {
-                return true;
-            }
-        } else if (recStack[u]) {
-            return true; // A cycle is detected
-        }
-    }
-
-    recStack[v] = false; // Remove the vertex from recursion stack
-    return false;
-}
-
-bool Graph::hasCycle() {
-    vector<bool> visited(V, false);
-    vector<bool> recStack(V, false);
-
-    for (int i = 0; i < V; i++) {
-        if (!visited[i]) {
-            if (dfs(i, visited, recStack)) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
 int main() {
-    Graph g(4);
+    Graph g(5); // Create a graph with 5 vertices
+
     g.addEdge(0, 1);
     g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
+    g.addEdge(2, 0); // Adding a cycle here (0->1->2->0)
+    g.addEdge(1, 3);
+    g.addEdge(3, 4);
 
     if (g.hasCycle()) {
-        cout << "Cycle detected in the directed graph" << endl;
+        std::cout << "Graph contains a cycle" << std::endl;
     } else {
-        cout << "No cycle detected in the directed graph" << endl;
+        std::cout << "Graph does not contain a cycle" << std::endl;
     }
 
     return 0;
