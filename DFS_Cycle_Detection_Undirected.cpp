5fcb8acedd8c8e2c8b73ebe59f669a01320f054f
MatthewBieda <matthew.bieda@gmail.com>
2024-08-29 11:12:26 +0100

    Cleaner implementation

--- DFS_Cycle_Detection_Undirected.cpp
+++ DFS_Cycle_Detection_Undirected.cpp
@@ -5,71 +5,65 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 class Graph {
-    int V; // Number of vertices
-    vector<vector<int>> adjList; // Adjacency list
+private:
+    std::vector<std::vector<int>> adjList;
+    std::vector<bool> visited;
 
-    bool dfs(int v, int parent, vector<bool> &visited);
+    bool dfs(int node, int parent) {
+        visited[node] = true;
+
+        for (int neighbor : adjList[node]) {
+            if (!visited[neighbor]) {
+                // Visit the neighbor recursively
+                if (dfs(neighbor, node)) {
+                    return true;
+                }
+            } else if (neighbor != parent) {
+                // If the neighbor is visited and is not the parent, we found a cycle
+                return true;
+            }
+        }
+        return false;
+    }
 
 public:
-    Graph(int V);
-    void addEdge(int v, int w);
-    bool hasCycle();
+    Graph(int vertices) : adjList(vertices), visited(vertices, false) {}
+
+    void addEdge(int u, int v) {
+        adjList[u].push_back(v);
+        adjList[v].push_back(u); // Since the graph is undirected
+    }
+
+    bool hasCycle() {
+        // Check for each component
+        for (int i = 0; i < adjList.size(); ++i) {
+            if (!visited[i]) {
+                if (dfs(i, -1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 };
 
-Graph::Graph(int V) {
-    this->V = V;
-    adjList.resize(V);
-}
-
-void Graph::addEdge(int v, int w) {
-    adjList[v].push_back(w);
-    adjList[w].push_back(v); // Since the graph is undirected
-}
-
-bool Graph::dfs(int v, int parent, vector<bool> &visited) {
-    visited[v] = true;
-
-    for (int u : adjList[v]) {
-        if (!visited[u]) {
-            if (dfs(u, v, visited)) {
-                return true;
-            }
-        } else if (u != parent) {
-            return true; // A cycle is detected
-        }
-    }
-    return false;
-}
-
-bool Graph::hasCycle() {
-    vector<bool> visited(V, false);
-
-    for (int i = 0; i < V; i++) {
-        if (!visited[i]) {
-            if (dfs(i, -1, visited)) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
 int main() {
-    Graph g(5);
+    Graph g(5); // Create a graph with 5 vertices
+
     g.addEdge(0, 1);
     g.addEdge(1, 2);
-    g.addEdge(2, 0);
+    g.addEdge(2, 0); // Adding a cycle here (0-1-2-0)
+    g.addEdge(1, 3);
     g.addEdge(3, 4);
 
     if (g.hasCycle()) {
-        cout << "Cycle detected in the undirected graph" << endl;
+        std::cout << "Graph contains a cycle" << std::endl;
     } else {
-        cout << "No cycle detected in the undirected graph" << endl;
+        std::cout << "Graph does not contain a cycle" << std::endl;
     }
 
     return 0;
 }
+
 
