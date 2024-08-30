cc29f2138d0d7800ce5ad952171504a4188b6052
MatthewBieda <matthew.bieda@gmail.com>
2024-08-30 14:12:40 +0100

    Cleaner version

--- Backtracking_Constraints.cpp
+++ Backtracking_Constraints.cpp
@@ -6,30 +6,31 @@
 class Solution {
 public:
     std::vector<std::vector<int>> combine(int n, int k) {
-        std::vector<std::vector<int>> answer;
-        std::vector<int> current;
+        std::vector<std::vector<int>> all_combinations; // To store all possible combinations
+        std::vector<int> current_combination; // To store the current combination being built
 
-        backtrack(current, 1, answer, k, n);
-
-        return answer;
+        // Start backtracking from number 1
+        backtrack(1, current_combination, all_combinations, n, k);
+        
+        return all_combinations;
     }
 
 private:
-    void backtrack(std::vector<int>& curr, int firstNum, std::vector<std::vector<int>>& answer, int k, int n) {
-
-        // constraint that prunes the search space
-        if (curr.size() == k) {
-            answer.push_back(curr);
+    void backtrack(int next_start, std::vector<int>& current_combination, std::vector<std::vector<int>>& all_combinations, const int n, const int k) {
+        // If the current combination has reached the desired size k, add it to the results
+        if (current_combination.size() == k) {
+            all_combinations.push_back(current_combination);
             return;
         }
 
-        for (int num = firstNum; num <= n; ++num) {
-            curr.push_back(num);
-            backtrack(curr, num + 1, answer, k, n);
-            curr.pop_back();
+        // Explore further combinations starting from next_start to n
+        for (int i = next_start; i <= n; ++i) {
+            current_combination.push_back(i); // Add i to the current combination
+            // Recursively build the rest of the combination
+            backtrack(i + 1, current_combination, all_combinations, n, k);
+            // Backtrack: remove the last element to explore other possibilities
+            current_combination.pop_back();
         }
-
-        return;
     }
 };
 
